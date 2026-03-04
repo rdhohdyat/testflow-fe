9580d533436168c48a96cfc8da59ac811ef6f014
rdhohdyat <rdho.hdyat@gmail.com>
2026-03-04 11:47:44 +0700

    change create project environtment

--- src/components/create-project.tsx
+++ src/components/create-project.tsx
@@ -17,7 +17,7 @@ export default function CreateProject({ onProjectCreated }: { onProjectCreated: 
     if (!name) return;
     setLoading(true);
     try {
-      const response = await fetch("http://127.0.0.1:8000/projects/", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/projects/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, description }),
